b0f53bd4af4c122b7cee57bf355efa5387e2ec53
aarush <aarushashok2011@gmail.com>
2025-05-02 16:32:12 +0530

    Project -8

---
+++ Class 11/Factorial.py
@@ -0,0 +1,9 @@
+#Factorial
+num = int(input("Enter a positive number:"))
+if num <0:
+    print("Factorial is not defined for negative numbers")
+else:
+    for num in range (1,21):
+        factorial = 1
+        for x in range
+        print(f"Factorial of {num} is : {factorial}")
