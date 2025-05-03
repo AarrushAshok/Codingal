546a7c6216fcdf33815ce2fc291eb47d93ec252b
aarush <aarushashok2011@gmail.com>
2025-05-03 10:44:36 +0530

    Factorial

--- Class 11/Factorial.py
+++ Class 11/Factorial.py
@@ -6,6 +6,6 @@ if num <0:
     print("Factorial is not defined for negative numbers")
 else:
     for num in range (1,num+1):
-        factorial *= num    
+        factorial *= num
 
 print(f"Factorial of {num} is : {factorial}")
