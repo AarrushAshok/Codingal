10fd539b1c1a19238e2367cf0d39ba1b9d7003cd
aarush <aarushashok2011@gmail.com>
2025-05-05 18:10:46 +0530

    pro

---
+++ Codingal/Class 12/Half pyramid.py
@@ -0,0 +1,7 @@
+#Half pyramid
+num = int(input("Enter a number to display half pyramid:"))
+
+for i in range(num):
+    for j in range(i+1):
+        print("*", end="")
+    print()
---
+++ Codingal/Class 12/N-Forest.py
@@ -0,0 +1,9 @@
+#N-Forest
+num = int(input("Enter a number to display stars:"))
+
+for i in range(num):
+    for j in range(num):
+        print("*",end="")
+    print()
+
+    
