d27e962fe09b664691e87c5f9c56cd5d12b0d116
aarush <aarushashok2011@gmail.com>
2025-05-06 16:11:43 +0530

    project

--- Codingal/Class 12/Project -12.py
+++ Codingal/Class 12/Project -12.py
@@ -1,8 +1,7 @@
 #Half pyramid of numbers
 num = int(input("Enter a number to display half pyramid of numbers:"))
 
-for i in range(num):
-    print(f"{i}","end=")
+for i in range(1,num+1):
     for j in range(i):
-        print("i", end="")
+        print(f"{i}",end="")
     print()
