d792459c2a16026c330c056b5f6fd9e271d9afc5
aarush <aarushashok2011@gmail.com>
2025-05-07 14:18:06 +0530

    pro

---
+++ Codingal/Class 13/Project -13.py
@@ -0,0 +1,15 @@
+#Multiplication Table of 17
+i =1
+print(f"Multiplication Table of 17:")
+for i in range(1,11)
+print()
+print(f""{17}*{i})
+
+number = 17
+i = 1
+
+print(f"Multiplication Table of {number}")
+while i <= 12:
+    result = number * i
+    print(f"{number} x {i} = {result}")
+    i += 1
