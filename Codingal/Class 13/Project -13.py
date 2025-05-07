b4847777ba0ca2db5520ade2346da1cb13d3b5bf
aarush <aarushashok2011@gmail.com>
2025-05-07 18:31:33 +0530

    pro\

--- Codingal/Class 13/Project -13.py
+++ Codingal/Class 13/Project -13.py
@@ -1,15 +1,9 @@
 #Multiplication Table of 17
+num =17
 i =1
 print(f"Multiplication Table of 17:")
-for i in range(1,11)
-print()
-print(f""{17}*{i})
+while i <= 15:
+    print(f"{num}*{i}={num*i}")
+    i +=1
 
-number = 17
-i = 1
-
-print(f"Multiplication Table of {number}")
-while i <= 12:
-    result = number * i
-    print(f"{number} x {i} = {result}")
-    i += 1
+print() 
