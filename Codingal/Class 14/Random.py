92b1b789ba663ae1442d263b8c6b0e1d07829aff
aarush <aarushashok2011@gmail.com>
2025-05-09 09:01:00 +0530

    '

--- Codingal/Class 14/Random.py
+++ Codingal/Class 14/Random.py
@@ -1,34 +1,26 @@
 #Random
 import turtle
 import random
+import time
 
 screen = turtle.Screen()
 screen.bgcolor("white")
 
-# Setup the turtle
 x = turtle.Turtle()
-x.speed(0)  # Fastest drawing speed
-x.pensize(3)  # Thicker lines
-x.hideturtle()  # Hide turtle shape so only stars are visible
+x.speed(0)
+x.pensize(1)
 
 colorList = ["red", "green", "blue", "yellow", "purple"]
 
-# Draw 100 stars at random positions
 for i in range(100):
     x.penup()
-    # Make sure the turtle is within visible range of the screen
     x.goto(random.randint(-350, 350), random.randint(-250, 250))
     x.pendown()
 
-    # Set the color for the star
     x.color(colorList[i % len(colorList)])
 
-    # Draw a 5-pointed star
     for _ in range(5):
-        x.forward(50)  # Increased size of each star
+        x.forward(50)
         x.left(144)
-
-    time.sleep(0.05)  # Small delay to see the star drawing
-
-# Finish the drawing and hold the window open
+        
 turtle.done()
