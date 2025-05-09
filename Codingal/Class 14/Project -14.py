cb12a6d4caed9e6d0cefaaa5d22daa981641bb91
aarush <aarushashok2011@gmail.com>
2025-05-09 09:07:42 +0530

    p

--- Codingal/Class 14/Project -14.py
+++ Codingal/Class 14/Project -14.py
@@ -1,25 +1,25 @@
-#Random
+#Hexagon using turtle
 import turtle
-import random
 
 screen = turtle.Screen()
 screen.bgcolor("white")
 
 x = turtle.Turtle()
 x.speed(0)
-x.pensize(1)
+x.pensize(2)
 
 colorList = ["red", "green", "blue", "yellow", "purple"]
 
-for i in range(100):
+for i in range(5):
     x.penup()
-    x.goto(random.randint(-350, 350), random.randint(-250, 250))
+    x.goto(-200 + (i * 100), 0)
     x.pendown()
 
     x.color(colorList[i % len(colorList)])
 
-    for i in range(5):
+
+    for i in range(6):
         x.forward(50)
-        x.left(144)
+        x.left(60)
 
 turtle.done()
