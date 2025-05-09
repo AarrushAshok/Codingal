a2b8d3a0981b0472a6ced1186c7d3b1b5917cdbf
aarush <aarushashok2011@gmail.com>
2025-05-09 09:17:30 +0530

    p

--- Codingal/Class 14/Project -14.py
+++ Codingal/Class 14/Project -14.py
@@ -2,20 +2,17 @@
 import turtle
 import random
 
-screen = turtle.Screen()
-screen.bgcolor("white")
-
 x = turtle.Turtle()
 x.speed(0)
-x.pensize(2)
+x.pensize(3)
 
 for i in range(5):
     x.penup()
-    x.goto(random.randit(-300,300),random.randint(-300,300))
+    x.goto(random.randint(-300, 300), random.randint(-300, 300))
     x.pendown()
 
-for i in range(6):
-    x.forward(50)
-    x.left(60)
+    for i in range(6):
+        x.forward(50)
+        x.left(60)
 
 turtle.done()
