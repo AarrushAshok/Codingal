58c3318636550f194feb2c9a0a2992693dc0d660
aarush <aarushashok2011@gmail.com>
2025-05-09 09:28:43 +0530

    p

--- Codingal/Class 15/Project -15.py
+++ Codingal/Class 15/Project -15.py
@@ -1,7 +1,6 @@
 #Calculating circumference using function
 def calculateCircumference(radius):
-    2 * 3.14 * radius
+    print(2 * 3.14 * radius)
 
 radius = float(input("Enter the radius of the circle:"))
 circumference = calculateCircumference(radius)
-print("The circumference of the circle is:",circumference)
