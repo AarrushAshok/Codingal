57e31b1a79e5a273b9b8becfa85080ceb2be5068
aarush <aarushashok2011@gmail.com>
2025-05-26 12:20:43 +0530

    ppp

--- Codingal/Class 18/Project -18.py
+++ Codingal/Class 18/Project -18.py
@@ -7,18 +7,3 @@ def get_temperature():
         print(f"Temperature entered: {temp}°C")
     except ValueError as ve:
         print("Error:", ve)
-
-def check_string_length(s):
-    if len(s) < 3:
-        raise ValueError("String length must be at least 3 characters.")
-    return "String length is acceptable."
-
-# Using the functions
-get_temperature()
-
-try:
-    user_input = input("Enter a string: ")
-    result = check_string_length(user_input)
-    print(result)
-except ValueError as e:
-    print("Error:", e)
