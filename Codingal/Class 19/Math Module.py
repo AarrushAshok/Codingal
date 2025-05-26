bee36f2fe25af9525eb0971a2deae6207dc774d2
aarush <aarushashok2011@gmail.com>
2025-05-26 16:56:43 +0530

    pp

--- Codingal/Class 19/Math Module.py
+++ Codingal/Class 19/Math Module.py
@@ -1,1 +1,11 @@
 #Math Module
+import math
+
+num = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+
+print("Square root :",math.sqrt(num))
+print("Exponential :",math.e)
+print("Pi :",math.pi)
+print("Factorial :",math.factorial(num))
+print("Power :",math.pow(num,num2))
---
+++ Codingal/Class 19/Number Game.py
@@ -0,0 +1,2 @@
+#Number Guessing Game
+computerChoice = random.randint(1,100)
