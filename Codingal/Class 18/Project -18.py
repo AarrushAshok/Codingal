cfbb22866d02088aa691459f6060c3635f6a9b37
aarush <aarushashok2011@gmail.com>
2025-05-26 12:22:49 +0530

    pp

--- Codingal/Class 18/Project -18.py
+++ Codingal/Class 18/Project -18.py
@@ -1,11 +1,8 @@
 #Temperature
-def get_temperature():
-    try:
+try:
         temp = float(input("Enter temperature in Celsius: "))
         if temp < -273.15:
             raise ValueError("Temperature cannot be below -273.15°C")
         print(f"Temperature entered: {temp}°C")
     except ValueError as ve:
         print("Error:", ve)
-
-get_temperature()
