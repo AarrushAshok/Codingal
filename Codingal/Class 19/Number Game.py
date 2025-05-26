7492ed4fac6a2db5bff1fa860073a88bf4452e85
aarush <aarushashok2011@gmail.com>
2025-05-26 17:01:11 +0530

    pp

--- Codingal/Class 19/Number Game.py
+++ Codingal/Class 19/Number Game.py
@@ -1,2 +1,13 @@
 #Number Guessing Game
+import random
+print("Welcome to number guessing game!!!!")
 computerChoice = random.randint(1,100)
+
+while True:
+    userGuess = int(input("Enter your guessed number:"))
+    if userGuess == computerChoice:
+        print("You Won!!!")
+    elif userGuess > computerChoice:
+        print("Number is big. Keep guessing")
+    elif userGuess < computerChoice:
+        print("Number is small.Keep guessing")
