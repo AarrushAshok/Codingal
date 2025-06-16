7eb8ffc3918e4381740d219a2fd8f567223f82ab
aarush <aarushashok2011@gmail.com>
2025-06-16 19:31:21 +0530

    ppp

--- Codingal/Class 19/Project -19.py
+++ Codingal/Class 19/Project -19.py
@@ -2,21 +2,33 @@
 import random
 print("Welcome to number guessing game!!!!")
 print("Max number of attempts will be 5!!")
-computerChoice = random.randint(1,100)
-maxAttempts = 5
-attempts = 0
 
-while attempts < maxAttempts:
-    userGuess = int(input("Enter your guessed number between 1 to 100:"))
-    attempts += 1
-    if userGuess == computerChoice:
-        print("You Won!!!")
+def playGame():
+    computerChoice = random.randint(1,100)
+    maxAttempts = 5
+    attempts = 0
+    while attempts < maxAttempts:
+        userGuess = int(input("Enter your guessed number between 1 to 100:"))
+        attempts += 1
+        if userGuess == computerChoice:
+            print("You Won!!!")
+            break
+        elif userGuess > computerChoice:
+            print("Number is big. Keep guessing")
+        elif userGuess < computerChoice:
+            print("Number is small.Keep guessing")
+
+    if attempts == maxAttempts and userGuess != computerChoice:
+        print("You've used all attempts! The number was:",computerChoice)
+        print("Good Luck next time!")
+
+while True:
+    print("1.Play Game")
+    print("2.End Game")
+    choice = int(input("Enter your choice:"))
+    attemnpts = 0
+    if choice == 1:
+        playGame()
+    if choice == 2:
+        print("Thank You, Come back Soon!!")
         break
-    elif userGuess > computerChoice:
-        print("Number is big. Keep guessing")
-    elif userGuess < computerChoice:
-        print("Number is small.Keep guessing")
-
-if attempts == maxAttempts and userGuess != computerChoice:
-    print("You've used all attempts! The number was:",computerChoice)
-    print("Good Luck next time!")
