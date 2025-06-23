4392907abcc79b2ed8f794a682415610867b5935
aarush <aarushashok2011@gmail.com>
2025-06-23 19:43:04 +0530

    ppp

--- Codingal/Class 21/Lists.py
+++ Codingal/Class 21/Lists.py
@@ -1,17 +1,48 @@
 #Lists
-ages = [12,13,14,15]
-print(ages)
-print(ages[0])
-print(ages[1])
-print(ages[2])
-print(ages[3])
+todo = [] #Empty List
 
-#Adding Age
-newAge = int(input("Enter new age:"))
-ages.append(newAge)
-print(ages)
+def addTask():
+    task = input("Enter a new task to add:")
+    todo.append(task)
+    print("Task added successfully")
 
-#Deleting Age
-ages.pop()
-ages.pop()
-print(ages)
+def viewTask():
+    print("Viewing all tasks")
+    count = 1
+    for task in todo:
+        print(count.task)
+        count += 1
+
+def deleteTask():
+    task = input("Enter a task to delete:")
+    if task in todo:
+        todo.remove(task)
+        print("Task deleted successfully")
+    else:
+        print("Task not found")
+def searchTask():
+    task = input("Enter a task to search:")
+    if task in todo:
+        print(f"{task} found")
+    else:
+        print("Task not found")
+
+menu = """1. Add a new task
+          2. View all task
+          3. Delete a task
+          4. Search for a task
+          5. Exit""" #Formatted String
+
+while True:
+    print(menu)
+    choice = int(input("Enter your choice:"))
+    if choice == 1:
+        addTask()
+    elif choice == 2:
+        viewTask()
+    elif choice == 3:
+        deleteTask()
+    elif choice == 4:
+        searchTask()
+    elif choice == 5:
+        print("Exiting the App")
