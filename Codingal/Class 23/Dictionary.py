0a4b8bf97aa883018061ed0063d1ba63d5272087
aarush <aarushashok2011@gmail.com>
2025-07-07 19:51:43 +0530

    pp

---
+++ Codingal/Class 23/Dictionary.py
@@ -0,0 +1,11 @@
+#Dictionary
+Dictionary ={
+"Sky": "Akash",
+"Aarush": "First ray of the sun",
+"Aman": "Peace",
+"Konnichiwa": "Hello",
+}
+
+print(Dictionary)
+word = input("Enter a word:")
+print(f"Meaning of {word} is",Dictionary[word])
