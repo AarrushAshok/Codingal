cd29b1a8ca4ec8278821a4ce893c982ced964f49
aarush <aarushashok2011@gmail.com>
2025-07-28 19:23:58 +0530

    ppp

---
+++ Codingal/Class 25/List Comprehension.py
@@ -0,0 +1,10 @@
+#List Comprehension
+list1 = [13,45,21,78,87,35,65,78,98,23,45,67,89]
+print("Original list:",list1)
+
+listOfSquare = [x**2 for x in list1]
+evenNumber = [x for x in list1 if x%2 == 0]
+evenNumber = [x for x in evenNumber]
+
+print("List of squares:",listOfSquare)
+print("Even Numbers:",evenNumber)
