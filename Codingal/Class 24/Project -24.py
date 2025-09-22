bf4832c5da6f68f49855510471e966a6fdc20fe7
aarush <aarushashok2011@gmail.com>
2025-09-22 20:01:42 +0530

    ppp

--- Codingal/Class 24/Project -24.py
+++ Codingal/Class 24/Project -24.py
@@ -1,10 +1,11 @@
 #Symmetric Difference of two Sets
+#In colours
 Set1 = {'blue','green'}
 Set2 = {'blue','yellow'}
 symmetric_difference = Set1.symmetric_difference(Set2)
 print("Symmetric Difference:", symmetric_difference)
 
-
+#In Numbers
 Set1 = {1, 2, 3, 4, 5}
 Set2 = {1, 5, 6, 7, 8, 9}
 symmetric_difference = Set1.symmetric_difference(Set2)
