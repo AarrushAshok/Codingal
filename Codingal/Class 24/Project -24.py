fd70804813088f3551ce70b9e36b24e212de0719
aarush <aarushashok2011@gmail.com>
2025-09-22 19:58:14 +0530

    ppp

---
+++ Codingal/Class 24/Project -24.py
@@ -0,0 +1,7 @@
+#Symmetric Difference of two Sets
+Set1 = {'blue','green'}
+Set2 = {'blue','yellow'}
+
+symmetric_difference = Set1.symmetric_difference(Set2)
+
+print("Symmetric Difference:", symmetric_difference)
