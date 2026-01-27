9e49cbb500e2cc64407a591e4f296e4a5701c8fd
aarush <aarushashok2011@gmail.com>
2026-01-27 19:25:59 +0530

    nbgg

--- Codingal/Class 40/Tkinter.py
+++ Codingal/Class 40/Tkinter.py
@@ -1,1 +1,11 @@
 #Tkinter
+from tkinter import*
+window = Tk()
+window.title("Intro Tkinter")
+window.geometry("300x200")
+
+#Frame:
+frame = Frame(master=window,bg="lightblue",height=390,width=690)
+frame.pack()
+
+window.mainloop()
