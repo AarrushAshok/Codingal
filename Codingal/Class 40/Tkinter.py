6408e1b31fa6377a86a812339ceb15a9a63a66f2
aarush <aarushashok2011@gmail.com>
2026-01-27 19:41:59 +0530

    gdvyuolkdfa

--- Codingal/Class 40/Tkinter.py
+++ Codingal/Class 40/Tkinter.py
@@ -2,10 +2,20 @@
 from tkinter import*
 window = Tk()
 window.title("Intro Tkinter")
-window.geometry("300x200")
+window.geometry("700x400")
 
-#Frame:
-frame = Frame(master=window,bg="lightblue",height=390,width=690)
-frame.pack()
+def f1():
+    print("Welcome to Tkinter")
+
+button1 = Button(master=window,bg="green",text="submit",fg="black",command=f1,borderwidth=10,relief="ridge")
+button2 = Button(master=window,bg="green",text="submit",fg="black",command=f1,borderwidth=10,relief="sunken")
+button3 = Button(master=window,bg="green",text="submit",fg="black",command=f1,borderwidth=10,relief="groove")
+button4 = Button(master=window,bg="green",text="submit",fg="black",command=f1,borderwidth=10,relief="raised")
+
+button1.pack()
+button2.pack()
+button3.pack()
+button4.pack()
+
 
 window.mainloop()
