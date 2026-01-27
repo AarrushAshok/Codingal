b619a3728f498eb6ac13ae97808e5453cc13571b
aarush <aarushashok2011@gmail.com>
2026-01-27 19:52:31 +0530

    ji

--- Codingal/Class 40/Tkinter_Button and Frame.py
+++ Codingal/Class 40/Tkinter_Button and Frame.py
@@ -1,1 +1,24 @@
 #Tkinter Button and Frame
+from tkinter import*
+window = Tk()
+window.title("Intro Tkinter")
+window.geometry("700x400")
+
+def f1():
+    helloText.config(text="Hi")
+
+helloText = Label(master=window,text="hello",bg="blue",fg="green")
+helloText.pack()
+
+frame1 = Frame(master=window,bg="black",height=200,width=100)
+frame1.pack(side=TOP)
+
+frame2 = Frame(master=window,bg="green",height=400,width=200)
+frame2.pack(side=LEFT)
+
+frame3 = Frame(master=window,bg="red",height=100,width=300)
+frame3.pack(side=RIGHT)
+
+button1 = Button(master=window,bg="green",text="submit",fg="black",command=f1,borderwidth=10,relief="ridge")
+
+window.mainloop()
