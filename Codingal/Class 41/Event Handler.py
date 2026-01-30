24a871c65b474cd4a783cd3489a463ff07200dd3
aarush <aarushashok2011@gmail.com>
2026-01-30 20:27:11 +0530

    hvuyvw

--- Codingal/Class 41/Event Handler.py
+++ Codingal/Class 41/Event Handler.py
@@ -12,11 +12,8 @@ def handle_click(event):
     print("Mouse first button was clicked")
 def handle_click2(event):
     print("Mouse second button was clicked")
-def handle_click3(event):
-    print("Mouse third button was clicked")
 
 win.bind("<Button-1>",handle_click)
 win.bind("<Button-2>",handle_click2)
-win.bind("<Button-3>",handle_click3)
 
 win.mainloop()
