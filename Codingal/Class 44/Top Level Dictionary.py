98e5bf3a1e026f2a84c58788d31fd1e677adc8a3
aarush <aarushashok2011@gmail.com>
2026-01-31 16:33:57 +0530

    oijtou

--- Codingal/Class 44/Top Level Dictionary.py
+++ Codingal/Class 44/Top Level Dictionary.py
@@ -1,10 +1,20 @@
 #Top Level Dictionary
 from tkinter import *
+import requests
 win=Tk()
 win.geometry("400x500")
 win.title("Dictionary App")
 
-entry = Entry(win)
-entry.pack()
+entry = Entry(win,font=("Arial",15))
+entry.pack(pady=10)
+
+def Search():
+    pass
+
+searchButton = Button(win,text="Search",command=Search)
+searchButton.pack()
+
+meaning = Label(win,text="Meaning will Appear Here:")
+meaning.pack(pady=10)
 
 win.mainloop()
